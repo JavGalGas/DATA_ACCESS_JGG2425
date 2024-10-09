7394826378e312c0a28fda7bc0d152c39bf103f0
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-10-09 21:06:31 +0200

    commit 19

---
+++ UNIT_2/Exercise822/src/FileFormatConverter.java
@@ -0,0 +1,10 @@
+public class FileFormatConverter {
+//Modify the fuctions
+    public static void convertObjToXml(String objFilePath, String xmlFilePath) {
+
+    }
+
+    public static void convertXmlToObj(String xmlFilePath, String objFilePath) {
+
+    }
+}
--- UNIT_2/Exercise822/src/Main.java
+++ UNIT_2/Exercise822/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose an option: ");
+        System.out.println("1. Convert OBJ to XML");
+        System.out.println("2. Convert XML to OBJ");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            FileFormatConverter.convertObjToXml("contacts.obj", "contacts.xml");
+        } else if (choice == 2) {
+            FileFormatConverter.convertXmlToObj("contacts.xml", "contacts.obj");
+        } else {
+            System.out.println("Invalid choice");
+        }
     }
 
 }
