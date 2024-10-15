96d8cf823bf6d152a5d71db56d5d1ad483c13e14
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-10-15 20:46:17 +0200

    commit 24

--- UNIT_3/VThisTute/src/main/java/org/jgg2425da/Main.java
+++ UNIT_3/VThisTute/src/main/java/org/jgg2425da/Main.java
@@ -8,18 +8,25 @@ public class Main {
         String URL = "jdbc:postgresql://localhost:5432/JavierGGdb";
         String USER = "postgres";
         String PASS = "postgres";
-        try (Connection con = DriverManager.getConnection(URL, USER, PASS)) {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
             Statement statement = con.createStatement();
-            String SQLsentence = "SELECT * FROM subjects ORDER BY code";
+            String SQLsentence = "SELECT code, name, year FROM subjects ORDER BY code";
             ResultSet rs = statement.executeQuery(SQLsentence);
-            System.out.println("Code" + "\t" + "Name");
+            System.out.println("Code" + "\t" + "Name" + "\t" + "Year");
             System.out.println("-----------------------------------------");
             while (rs.next()) {
                 System.out.println(rs.getString(1) + "\t " +
-                        rs.getString(2));
+                        rs.getString(2) + "\t" + rs.getString(3));
             }
-        } catch (Exception ex) {
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
+        }
+        finally {
+            if (con != null) {
+                con.close();
+            }
         }
 
 
