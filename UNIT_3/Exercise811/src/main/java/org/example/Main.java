a2126d417cf78877167cbb6527e689633278fcdf
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-10-23 17:05:57 +0200

    commit 29

--- UNIT_3/Exercise811/src/main/java/org/example/Main.java
+++ UNIT_3/Exercise811/src/main/java/org/example/Main.java
@@ -3,44 +3,63 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     static final String URL = "jdbc:postgresql://localhost:5432/EmployeeDB";
     static final String USER = "postgres";
     static final String PASS = "postgres";
     static boolean isRunning = true;
+
+    static int deptNo;
+    static int empNo;
+    static String eName;
+    static String job;
+    static String dName;
+    static String loc;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         try ( Connection conn = DriverManager.getConnection(URL, USER, PASS)
         ) {
             conn.setAutoCommit(false);
             try {
+                //Set employee parameters
+                System.out.println("Provide a employee number: ");
+                empNo = scanner.nextInt();
+                System.out.println("");
+                //Check if deptno exists
                 PreparedStatement checkDeptNo = conn.prepareStatement("SELECT 1 FROM dept WHERE deptno = ?");
                 checkDeptNo.setInt(1, 40);
                 ResultSet resultSet = checkDeptNo.executeQuery();
                 boolean departmentExists = resultSet.next();
-                if (!departmentExists)
-                PreparedStatement deptNoStatement = conn.prepareStatement("SELECT deptno FROM dept ORDER BY deptno DESC LIMIT 1;");
-                resultSet = deptNoStatement.executeQuery();
-                int newDeptNo = resultSet.getInt(1) + 10;
-                PreparedStatement statement = conn.prepareStatement("INSERT INTO dept(deptno, dname, loc) VALUES(?,?,?)", Statement.RETURN_GENERATED_KEYS);
-                statement.setInt(1, newDeptNo);
-                statement.setString(2, "");
-                statement.setString(3, "");
-                statement.executeUpdate();
-                ResultSet keys = statement.getGeneratedKeys();
-                keys.next();
-                int courseCode = keys.getInt(1);
-                PreparedStatement statement2 = conn.prepareStatement("INSERT INTO employee(empno, ename, job, deptno) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+                resultSet.close();
+                if (!departmentExists) {
+                    //Set dept parameters besides deptno
+                    System.out.println("Provide a department name: ");
+                    dName = scanner.next();
+                    System.out.println("Provide a location: ");
+                    loc = scanner.next();
+                    scanner.nextLine();
+                    //Insert dept
+                    PreparedStatement statement = conn.prepareStatement("INSERT INTO dept(deptno, dname, loc) VALUES(?,?,?)");
+                    statement.setInt(1, deptNo);
+                    statement.setString(2, dName);
+                    statement.setString(3, loc);
+                    statement.executeUpdate();
+                }
+                //Insert employee
+                PreparedStatement statement2 = conn.prepareStatement("INSERT INTO employee(empno, ename, job, deptno) VALUES(?,?,?,?)");
                 statement2.setString(1, "Computer Systems Administration");
                 statement2.setString(2, "");
                 statement2.setString(3, "");
-                statement2.setInt(4,courseCode);
+                statement2.setInt(4, deptNo);
                 statement2.executeUpdate();
                 System.out.println("Transaction finished successfully!!");
                 conn.commit();
             }
-            catch( SQLException ex ) {
-                System.err.println( ex.getMessage() );
+            catch( SQLException sqlException ) {
+                System.err.println( sqlException.getMessage() );
                 conn.rollback();
             }
         } catch (SQLException exception) {
