15338d6b2185b16db39339a4695aace7ed38ffe4
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-11-12 20:45:41 +0100

    commit 42

---
+++ FA1/Practica1JavaFX_JGG/src/main/java/com/jgg/unit4/practica1javafx_jgg/LoginController.java
@@ -0,0 +1,11 @@
+package com.jgg.unit4.practica1javafx_jgg;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    protected void onLoginButtonClick() {
+        System.out.println("Hello Button Clicked!");
+    }
+}
