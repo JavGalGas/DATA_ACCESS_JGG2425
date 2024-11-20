4cd212b3d28f7e201cb245f8a465ab9c332ec591
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-11-20 20:51:00 +0100

    commit 48

--- FA1/Practica1JavaFX_JGG/src/main/java/com/jgg/unit4/practica1javafx_jgg/ProdOffersController.java
+++ FA1/Practica1JavaFX_JGG/src/main/java/com/jgg/unit4/practica1javafx_jgg/ProdOffersController.java
@@ -1,12 +1,17 @@
 package com.jgg.unit4.practica1javafx_jgg;
 
-import javafx.css.StyleClass;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
 
 public class ProdOffersController {
     @FXML
@@ -19,14 +24,51 @@ public class ProdOffersController {
     public HBox tab4;
 
     @FXML
-    public StyleClass custom_tab;
-    public ComboBox productCB;
+    public ComboBox<SellerProduct> productCB;
     public DatePicker fromDP;
     public DatePicker toDP;
     public TextField discountTF;
 
     @FXML
-    public void addOffer() {
+    public void onAddButtonClick() {
+        SellerProduct product = productCB.getSelectionModel().getSelectedItem();
+        LocalDate from = fromDP.getValue();
+        LocalDate to = toDP.getValue();
+        String discount = discountTF.getText();
+        addOffer(product, from, to, discount);
+    }
 
+    private void addOffer(SellerProduct product, LocalDate from, LocalDate to, String discount) {
+        //Product checks
+        if(product == null) {
+            SellerApplication.LOGGER.log(Level.SEVERE, "Product is null");
+        }
+        //From/To checks
+        //discount check
+        else{
+            try{
+                product.setOfferStartDate(from);
+                product.setOfferEndDate(to);
+                BigDecimal percentage = new BigDecimal(discount);
+                BigDecimal discountedPrice = product.getPrice().multiply(percentage).divide(new BigDecimal(100));
+                product.setOfferPrice(discountedPrice);
+                GenericClassCRUD<SellerProduct> sp_crud = new GenericClassCRUD<>(SellerProduct.class);
+                sp_crud.update(product);
+
+
+            } catch(Exception exception){
+                SellerApplication.LOGGER.log(Level.SEVERE, "An error occurred while adding a new offer", exception);
+            }
+        }
+    }
+
+
+
+    @FXML
+    private void initialize() {
+        // Get the current scene
+        Scene scene = SellerApplication.getAppStage().getScene();
+        // Add the CSS file
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("custom_tab.css")).toExternalForm());
     }
 }
