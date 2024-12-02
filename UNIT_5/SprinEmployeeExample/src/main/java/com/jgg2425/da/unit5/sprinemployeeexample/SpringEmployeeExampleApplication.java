10b0c51ad63a0b68f81148d0e5a8e6a5090da8cc
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-12-02 19:44:56 +0100

    commit 54

---
+++ UNIT_5/SprinEmployeeExample/src/main/java/com/jgg2425/da/unit5/sprinemployeeexample/SpringEmployeeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jgg2425.da.unit5.sprinemployeeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeExampleApplication.class, args);
+	}
+
+}
---
+++ UNIT_5/SprinEmployeeExample/src/test/java/com/jgg2425/da/unit5/sprinemployeeexample/SpringEmployeeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgg2425.da.unit5.sprinemployeeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
