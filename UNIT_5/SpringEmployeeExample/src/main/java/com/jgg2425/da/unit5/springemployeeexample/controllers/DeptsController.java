8bf1b22275e9d54d66eaad2baafa091e58e9727a
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-12-16 12:46:46 +0100

    commit 66

--- UNIT_5/SpringEmployeeExample/src/main/java/com/jgg2425/da/unit5/springemployeeexample/controllers/DeptsController.java
+++ UNIT_5/SpringEmployeeExample/src/main/java/com/jgg2425/da/unit5/springemployeeexample/controllers/DeptsController.java
@@ -3,6 +3,8 @@ package com.jgg2425.da.unit5.springemployeeexample.controllers;
 import com.jgg2425.da.unit5.springemployeeexample.models.dto.DeptDTO;
 import com.jgg2425.da.unit5.springemployeeexample.models.entities.DeptEntity;
 import com.jgg2425.da.unit5.springemployeeexample.services.DeptsService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,23 +25,23 @@ public class DeptsController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DeptEntity> findDeptById(@PathVariable(value = "id") int id) {
+    public ResponseEntity<DeptEntity> findDeptById(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return deptsService.findDeptById(id);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateDept(@RequestBody @Validated DeptEntity dept,
-                                                 @PathVariable(value = "id") int id) {
+                                        @NotNull @Min(0) @PathVariable(value = "id") int id) {
         return deptsService.updateDept(dept, id);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteDept(@PathVariable(value = "id") int id) {
+    public ResponseEntity<?> deleteDept(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return deptsService.deleteDept(id);
     }
 
     @PostMapping
-    public ResponseEntity<?> saveDept(@Validated @RequestBody DeptEntity dept) {
+    public ResponseEntity<?> saveDept(@NotNull @Validated @RequestBody DeptEntity dept) {
         return deptsService.saveDept(dept);
     }
 
@@ -49,7 +51,7 @@ public class DeptsController {
     }
 
     @GetMapping("/dto/{id}")
-    public ResponseEntity<DeptDTO> findDeptDTOById(@PathVariable(value = "id") int id) {
+    public ResponseEntity<DeptDTO> findDeptDTOById(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return deptsService.findDeptDTOById(id);
     }
 }
--- UNIT_5/SpringEmployeeExample/src/main/java/com/jgg2425/da/unit5/springemployeeexample/controllers/EmployeesController.java
+++ UNIT_5/SpringEmployeeExample/src/main/java/com/jgg2425/da/unit5/springemployeeexample/controllers/EmployeesController.java
@@ -3,6 +3,8 @@ package com.jgg2425.da.unit5.springemployeeexample.controllers;
 import com.jgg2425.da.unit5.springemployeeexample.models.entities.EmployeeEntity;
 import com.jgg2425.da.unit5.springemployeeexample.models.dto.EmployeeDTO;
 import com.jgg2425.da.unit5.springemployeeexample.services.EmployeesService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,22 +25,23 @@ public class EmployeesController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeeEntity> findEmployeeById(@PathVariable(value = "id") int id) {
+    public ResponseEntity<EmployeeEntity> findEmployeeById(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return employeesService.findEmployeeById(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateEmployee(@RequestBody EmployeeEntity employee,@PathVariable(value = "id") int id) {
+    public ResponseEntity<?> updateEmployee(@NotNull @RequestBody EmployeeEntity employee,
+                                            @NotNull @Min(0) @PathVariable(value = "id") int id) {
         return employeesService.updateEmployee(employee,id);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteEmployee(@PathVariable(value = "id") int id) {
+    public ResponseEntity<?> deleteEmployee(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return employeesService.deleteEmployee(id);
     }
 
     @PostMapping
-    public ResponseEntity<?> saveEmployee(@Validated @RequestBody EmployeeEntity employee) {
+    public ResponseEntity<?> saveEmployee(@NotNull @Validated @RequestBody EmployeeEntity employee) {
         return employeesService.saveEmployee(employee);
     }
 
@@ -48,7 +51,7 @@ public class EmployeesController {
     }
 
     @GetMapping("/dto/{id}")
-    public ResponseEntity<EmployeeDTO> findEmployeeDTOById(@PathVariable(value = "id") int id) {
+    public ResponseEntity<EmployeeDTO> findEmployeeDTOById(@NotNull @Min(0) @PathVariable(value = "id") int id) {
         return employeesService.findEmployeeDTOById(id);
     }
 
