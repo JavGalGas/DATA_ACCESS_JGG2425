c7f6ab7789f0405bccc0201b4ca58ec33a877ad0
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2025-01-28 20:45:51 +0100

    commit 85

---
+++ FA2/FinalActivity2/src/main/java/com/jgg2425/da/fa2/finalactivity2/FinalActivity2Application.java
@@ -0,0 +1,13 @@
+package com.jgg2425.da.fa2.finalactivity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalActivity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalActivity2Application.class, args);
+	}
+
+}
---
+++ FA2/FinalActivity2/src/test/java/com/jgg2425/da/fa2/finalactivity2/FinalActivity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgg2425.da.fa2.finalactivity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalActivity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
