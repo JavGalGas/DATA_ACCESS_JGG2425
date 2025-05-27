52e0a734bb9de6e5655f2970d39978d82eef7424
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2025-05-27 20:00:11 +0200

    commit "Testing and Documentation CUA AD 3"

--- CUA-JGG2425/Flights_JGG/src/test/java/com/jgg/catchup/flights_jgg/FlightServiceTests.java
+++ CUA-JGG2425/Flights_JGG/src/test/java/com/jgg/catchup/flights_jgg/FlightServiceTests.java
@@ -2,12 +2,11 @@ package com.jgg.catchup.flights_jgg;
 import com.jgg.catchup.flights_jgg.models.dao.IFlightDAO;
 import com.jgg.catchup.flights_jgg.models.dto.DropDownMenuOptionDTO;
 import com.jgg.catchup.flights_jgg.services.FlightService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
@@ -16,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = FlightService.class)
+@ExtendWith(MockitoExtension.class)
 class FlightServiceTests {
 
     @Mock
@@ -24,11 +23,6 @@ class FlightServiceTests {
 
     @InjectMocks
     private FlightService flightService;
-
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
 
     @Test
     void findAllSources_success() {
@@ -63,6 +57,95 @@ class FlightServiceTests {
         when(flightDAO.findAllSources()).thenReturn(Optional.empty());
 
         List<DropDownMenuOptionDTO> result = flightService.findAllSources();
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findAllDestinationsBySource_success() {
+        String origin = "CDG";
+        List<Object[]> daoReturn = Arrays.asList(
+                new Object[]{"JFK", "New York John F. Kennedy"},
+                new Object[]{"MMA", "p"}
+        );
+        when(flightDAO.findAllDestinationsBySource(origin)).thenReturn(Optional.of(daoReturn));
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllDestinationsBySource(origin);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("JFK", result.get(0).getCode());
+        assertEquals("New York John F. Kennedy", result.get(0).getTitle());
+        assertEquals("MMA", result.get(1).getCode());
+        assertEquals("p", result.get(1).getTitle());
+    }
+
+    @Test
+    void findAllDestinationsBySource_noResultsFromDAO() {
+        String origin = "CDG";
+
+        when(flightDAO.findAllDestinationsBySource(origin)).thenReturn(Optional.of(Collections.emptyList()));
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllDestinationsBySource(origin);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findAllDestinationsBySource_emptyOptionalFromDAO() {
+        String origin = "CDG";
+
+        when(flightDAO.findAllDestinationsBySource(origin)).thenReturn(Optional.empty());
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllDestinationsBySource(origin);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findAllFlightsBySourceAndDestination_success() {
+        String origin = "CDG";
+        String destination = "JFK";
+        List<Object[]> daoReturn = Arrays.asList(
+                new Object[]{"AB0004", "CDG -> JFK : Flight 1"},
+                new Object[]{"CD0003", "CDG -> JFK : Flight 2"}
+        );
+        when(flightDAO.findAllFlightsBySourceAndDestination(origin, destination)).thenReturn(Optional.of(daoReturn));
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllFlightsBySourceAndDestination(origin, destination);
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("AB0004", result.get(0).getCode());
+        assertEquals("CDG -> JFK : Flight 1", result.get(0).getTitle());
+        assertEquals("CD0003", result.get(1).getCode());
+        assertEquals("CDG -> JFK : Flight 2", result.get(1).getTitle());
+    }
+
+    @Test
+    void findAllFlightsBySourceAndDestination_noResultsFromDAO() {
+        String origin = "CDG";
+        String destination = "JFK";
+
+        when(flightDAO.findAllFlightsBySourceAndDestination(origin, destination)).thenReturn(Optional.of(Collections.emptyList()));
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllFlightsBySourceAndDestination(origin, destination);
+
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findAllFlightsBySourceAndDestination_emptyOptionalFromDAO() {
+        String origin = "CDG";
+        String destination = "JFK";
+
+        when(flightDAO.findAllFlightsBySourceAndDestination(origin, destination)).thenReturn(Optional.empty());
+
+        List<DropDownMenuOptionDTO> result = flightService.findAllFlightsBySourceAndDestination(origin, destination);
 
         assertNotNull(result);
         assertTrue(result.isEmpty());
